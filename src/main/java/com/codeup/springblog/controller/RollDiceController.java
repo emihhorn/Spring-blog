b2fb08dc96392ed331438bee58695131a5d09315
emihhorn <emilyhopehorn@gmail.com>
2021-05-03 15:20:31 -0500

    roll dice

--- src/main/java/com/codeup/springblog/controller/RollDiceController.java
+++ src/main/java/com/codeup/springblog/controller/RollDiceController.java
@@ -3,33 +3,26 @@ package com.codeup.springblog.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-public class DiceController {
+public class RollDiceController {
 
-    @GetMapping(value = "/roll-dice")
-    public String diceRoll() {
+    @GetMapping("/roll-dice")
+    public String showRoll() {
         return "roll-dice";
     }
 
-    @RequestMapping(value = "/roll-dice/{guess}", method = RequestMethod.GET)
-    public String diceRollResults(@PathVariable int guess, Model model) {
-        int randomNumberRolled = (int) (Math.random() * ((6 - 1) + 1) + 1);
-        model.addAttribute("guess", "This is your " + guess);
-        model.addAttribute("randomRoll", randomNumberRolled);
-        model.addAttribute("isCorrect", guess == randomNumberRolled);
-
-        if(guess == randomNumberRolled){
-            model.addAttribute("correctRoll", "You rolled correctly");
-            model.addAttribute("correctRoll", "You Win!!!\nYou guessed the number " + guess + " and rolled the number " + randomNumberRolled);
-        }
-        else {
-            model.addAttribute("incorrectRoll", "Pick again");
-            model.addAttribute("correctRoll", "You guessed the number " + guess + " and rolled the number " + randomNumberRolled);
-        }
-        return "roll-dice";
+    @GetMapping("/roll-dice/{n}")
+    public String guess(@PathVariable int n, Model model){
+        int randomNum = (int)(Math.random() * (7-1) +1);
+        model.addAttribute("randomNum", "The dice roll is: " + randomNum);
+        model.addAttribute("userGuess", "Your guess was: " + n);
+        boolean correctGuess = randomNum == n;
+        boolean incorrectGuess = randomNum != n;
+        model.addAttribute("correctGuess", correctGuess);
+        model.addAttribute("incorrectGuess", incorrectGuess);
+        return "/roll-dice";
     }
 
 }
