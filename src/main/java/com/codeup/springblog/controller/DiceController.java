2b35eb267a9e91168d862930b88f109565ba8286
emihhorn <emilyhopehorn@gmail.com>
2021-05-03 09:21:03 -0500

    mapping

--- src/main/java/com/codeup/springblog/controller/DiceController.java
+++ src/main/java/com/codeup/springblog/controller/DiceController.java
@@ -3,6 +3,7 @@ package com.codeup.springblog.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
