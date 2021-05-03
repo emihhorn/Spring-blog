bb2355c6f3eade1314196c6b33dd0dacee9c3590
emihhorn <emilyhopehorn@gmail.com>
2021-05-03 15:19:44 -0500

    added depend

---
+++ src/main/java/com/codeup/springblog/models/Post.java
@@ -0,0 +1,33 @@
+package com.codeup.springblog;
+
+public class Post {
+    
+        private String title;
+        private String body;
+
+        public Post(){
+        }
+
+        public Post(String title, String body) {
+            this.title = title;
+            this.body = body;
+        }
+
+        public String getTitle(){
+            return title;
+        }
+
+        public String getBody() {
+            return body;
+        }
+
+        public void setTitle(String title){
+            this.title = title;
+        }
+
+        public void setBody(String body) {
+            this.body = body;
+        }
+    }
+
+}
