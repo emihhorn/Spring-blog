c4c9a8bb20e9c79571d5e7dd3cbe21a93ab53e0a
emihhorn <emilyhopehorn@gmail.com>
2021-05-04 22:15:18 -0500

    dog repo

--- src/main/java/com/codeup/springblog/repositories/DogRepository.java
+++ src/main/java/com/codeup/springblog/repositories/DogRepository.java
@@ -1,2 +1,6 @@
-package com.codeup.springblog.repositories;public interface DogRepository {
+package com.codeup.springblog.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DogRepository extends JpaRepository {
 }
