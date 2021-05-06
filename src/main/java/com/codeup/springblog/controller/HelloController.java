481ca3075a47a6d2ec2a08e15d283d2362a53c48
emihhorn <emilyhopehorn@gmail.com>
2021-05-05 22:55:57 -0500

    hello update

--- src/main/java/com/codeup/springblog/controller/HelloController.java
+++ src/main/java/com/codeup/springblog/controller/HelloController.java
@@ -1,38 +1,50 @@
 package com.codeup.springblog.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HelloController {
     @GetMapping("/hello")
     @ResponseBody
     public String hello() {
-        return "hello";
+        return "Hello from Spring";
     }
 
-
-    @GetMapping("/hello/{Emily}")
-    @ResponseBody
-    public String sayHello(@PathVariable String Emily) {
-        return "Hello " + Emily + "!";
-    }
-
+//    @GetMapping("/hello{name}")
+//    @ResponseBody
+//    public String sayHello(@PathVariable String name) {
+//        return "Hello " + name + "!";
+//    }
 
 //    @RequestMapping(path = "/increment/{number}", method = RequestMethod.GET)
 //    @ResponseBody
 //    public String addOne(@PathVariable int number) {
-//        return number + " plus one is " + (number + 1) + "!";
+//        return number + " plus ons is " + (number + 1);
 //    }
 
+    @GetMapping("/hello/{name}")
+    public String sayHello(@PathVariable String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
 
-//    @GetMapping("/hello/{name}")
-//    public String sayHello(@PathVariable String name, Model model) {
-//        model.addAttribute("name", name);
-//        return "hello";
-//    }
+    @GetMapping("/join")
+    public String showJoinForm() {
+        return "join";
+    }
+
+    @PostMapping("/join")
+    public String joinCohort(@RequestParam(name = "cohort") String cohort, Model model) {
+        model.addAttribute("cohort", "Welcome to " + cohort + "!");
+        return "join";
+    }
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "welcome";
+    }
 
 
 }
