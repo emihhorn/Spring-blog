fde9b81a6185ce98d7c49f277d85199716024e75
emihhorn <emilyhopehorn@gmail.com>
2021-05-05 22:58:26 -0500

    post model

--- src/main/java/com/codeup/springblog/models/Post.java
+++ src/main/java/com/codeup/springblog/models/Post.java
@@ -1,33 +1,56 @@
 package com.codeup.springblog.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="posts")
 public class Post {
 
-        private String title;
-        private String body;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
 
-        public Post(){
-        }
+    @Column(nullable = false)
+    private String title;
 
-        public Post(String title, String body) {
-            this.title = title;
-            this.body = body;
-        }
+    @Column(columnDefinition = "TEXT NOT NULL")
+    private String body;
 
-        public String getTitle(){
-            return title;
-        }
-
-        public String getBody() {
-            return body;
-        }
-
-        public void setTitle(String title){
-            this.title = title;
-        }
-
-        public void setBody(String body) {
-            this.body = body;
-        }
+    public Post() {
     }
 
+    public Post(String title, String body) {
+        this.title = title;
+        this.body = body;
+    }
+
+    public Post(long id, String title, String body) {
+        this.id = id;
+        this.title = title;
+        this.body = body;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
 }
