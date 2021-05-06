5bfd904aaf3427066c125c4d3bde3bcb36a06bc4
emihhorn <emilyhopehorn@gmail.com>
2021-05-05 22:54:32 -0500

    update intellj

--- src/main/java/com/codeup/springblog/repositories/DogRepository.java
+++
@@ -1,6 +0,0 @@
-package com.codeup.springblog.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DogRepository extends JpaRepository {
-}
--- src/main/java/com/codeup/springblog/repositories/UserRepository.java
+++
@@ -1,2 +0,0 @@
-package com.codeup.springblog.repositories;public interface UserRepository {
-}
