9f765d239f510e3621875013b3ab5dffa3130a09
emihhorn <emilyhopehorn@gmail.com>
2021-05-05 22:56:17 -0500

    home update

--- src/main/java/com/codeup/springblog/controller/HomeController.java
+++ src/main/java/com/codeup/springblog/controller/HomeController.java
@@ -2,23 +2,11 @@ package com.codeup.springblog.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-
 public class HomeController {
     @GetMapping("/")
-    @ResponseBody
-    public String landing() {
-        return "This is the landing page!";
-    }
-
-
-    @GetMapping("/home")
     public String welcome() {
         return "home";
     }
-
-
-
 }
